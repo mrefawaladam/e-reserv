626f074d0ab91657397a27127a4a23d5cc222d67
berrylradianh <berrylhamesha@gmail.com>
2022-06-29 11:35:21 +0700

    Add first page

--- resources/views/firstpage.blade.php
+++ resources/views/firstpage.blade.php
@@ -7,8 +7,17 @@
             <div class="card-header p-3 text-center">
                 <h2>Online Order Menu</h2>
             </div>
-            <div class="card-body">
-            </div>
+            <div class="row">
+                <div class="col text-center">
+                    <label  class="pt-2"><h3>Scan QR Code</h3></label>
+                    <a class="nav-link" href="{{ url('scan-qrcode') }}"> <img class="rounded-circle" width="250" src="assets\img\files\scan.jpg" alt=" Scan QR Code"></a>
+                </div>
+                <div class="col text-center">
+                    <label class="pt-2"><h3>Menu</h3></label>
+                    <a class="nav-link" href="{{ url('menu-all') }}"> <img  class="rounded" width="200" src="assets\img\files\menu.jpg" alt="Menu"></a>
+                </div>
+              </div>
         </div>
+    </div>
 </div>
 @endsection
